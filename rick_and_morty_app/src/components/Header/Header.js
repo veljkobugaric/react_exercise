69456d3fe600142ccf0fc3231fbb517869f7a26c
BIT Student <classroom@bgit.rs>
2020-06-22 11:02:41 +0200

    Header changes

---
+++ rick_and_morty_app/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+
+const Header = () => {
+    return (
+        <div className='Header__wrapper'>
+            <h1 className='Header__title'> Rick&Morty Wiki</h1>
+        </div>
+    )
+}
+
+export default Header;
