e90546d1fd21abdde3ca9ab1ee7baac8a133524f
BIT Student <classroom@bgit.rs>
2020-06-25 15:21:36 +0200

    next-previous pages

--- rick_and_morty_app/src/components/Characters/Characters.js
+++ rick_and_morty_app/src/components/Characters/Characters.js
@@ -10,13 +10,14 @@ class Characters extends React.Component {
         super(props);
 
         this.state = {
-            characters: []
+            characters: [],
+            pages: [1, 2, 3, 4, 5, 6, 7],
+            page: 1
         }
     }
 
     componentDidMount() {
         const characters = new CharactersService();
-
         characters.get()
             // fetch('https://rickandmortyapi.com/api/character')
             //     .then(response => response.json())
@@ -24,37 +25,42 @@ class Characters extends React.Component {
     }
 
     renderCharacters() {
-        return this.state.characters.map((character, i) => {
-            return <CardView key={i} image={character.image} title={character.name} />
-        })
+        return
     }
 
-    onPageChange = (e) => {
+    onPageChange = (PageNumber) => {
         const chars = new CharactersService();
-
-        chars.getPage(e.target.textContent)
-            .then(page => this.setState({ characters: page.results }))
-
+        chars.getPage(PageNumber)
+            .then(page => this.setState({ characters: page.results, page: PageNumber }))
     }
 
-
+    nextPage = () => {
+        let pageNum = JSON.parse(this.state.page) + 1;
+        if (pageNum === 31) {
+            pageNum = 0;
+        }
+        this.onPageChange(pageNum)
+    }
+    prevPage = (e) => {
+        let pageNum = JSON.parse(this.state.page) - 1;
+        this.onPageChange(pageNum)
+    }
     render() {
         return (
             <div>
                 <div className='Characters__pagination_wrapper' >
                     <ul className='Characters__pagination'>
-                        <li><button id='firstChild'>&#60;</button></li>
-                        <li><button onClick={this.onPageChange}>1</button></li>
-                        <li><button onClick={this.onPageChange}>2</button></li>
-                        <li><button onClick={this.onPageChange}>3</button></li>
-                        <li><button onClick={this.onPageChange}>4</button></li>
-                        <li><button onClick={this.onPageChange}>5</button></li>
-                        <li><button id='lastChild'>&#62;</button></li>
-
+                        <li><button id='firstChild' onClick={this.prevPage}>&#60;</button></li>
+                        {this.state.pages.map((item, i) =>
+                            <li key={i}><button onClick={(e) =>
+                                this.onPageChange(e.target.textContent)}>{item}</button></li>)}
+                        <li><button id='lastChild' onClick={this.nextPage}>&#62;</button></li>
                     </ul>
                 </div>
                 <div className="Characters__wrapper">
-                    {this.renderCharacters()}
+                    {this.state.characters.map((character, i) => (
+                        <CardView key={i} image={character.image} title={character.name} />
+                    ))}
                 </div>
             </div>
 
